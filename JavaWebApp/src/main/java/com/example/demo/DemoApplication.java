78a1ed22d9ba8dba0c463a44bc7042d716e9b467
Showmik Bose <showmikbose@Showmiks-MacBook-Air.local>
2023-04-27 05:58:17 +0530

    Added comment to demoapplication.java

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//This is the start of the demo application
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
